e2aab1cb1246d7ce84e821fa1399697d6f1d0a64
Dan Lichty <djlichty@gmail.com>
2011-06-27 01:00:53 -0400

    add basic HTTP authentication

--- mcpanel.rb
+++ mcpanel.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  username == 'mcpanel' and password == 'password'
+end
+
 get '/' do
   "Hello from Sinatra on Heroku!"
 end
