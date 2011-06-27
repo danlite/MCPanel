b17d63372815ba5c7e7ce692fc4ee93d0dd58a60
Dan Lichty <djlichty@gmail.com>
2011-06-27 14:09:13 -0400

    use config password for basic auth; show EC2 instance status; button to start server

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
 
 gem 'sinatra'
+gem 'amazon-ec2', :require => 'aws'
+gem 'haml'
--- mcpanel.rb
+++ mcpanel.rb
@@ -1,10 +1,33 @@
 require 'rubygems'
 require 'sinatra'
+require 'aws'
+require 'haml'
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  username == 'mcpanel' and password == 'password'
+  username == 'mcpanel' and password == ENV['MCPANEL_AUTH_PASSWORD']
+end
+
+helpers do
+  def ec2_handle
+    AWS::EC2::Base.new(:access_key_id => ENV['AWS_KEY'], :secret_access_key => ENV['AWS_SECRET'])
+  end
 end
 
 get '/' do
-  "Hello from Sinatra on Heroku!"
+  ec2 = ec2_handle
+  
+  result = ec2.describe_instances(:instance_id => ENV['MC_EC2_INSTANCE_ID'])
+  instance = result.reservationSet.item.first.instancesSet.item.first
+  @state = instance.instanceState.name
+  
+  haml :index
+end
+
+get '/start_server' do
+  ec2_handle.start_instances(:instance_id => ENV['MC_EC2_INSTANCE_ID'])
+  "Server has been started. It will be ready shortly."
+end
+
+get '/stylesheet.css' do
+  sass :stylesheet
 end
