511869d0eadc23adff928dc344d6f304934c18ac
Dan Lichty <djlichty@gmail.com>
2011-10-18 15:23:01 -0400

    simpler login

--- mcpanel.rb
+++ mcpanel.rb
@@ -4,8 +4,16 @@ require 'AWS'
 require 'haml'
 require 'sass'
 
-use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  username == 'mcpanel' and password == ENV['MCPANEL_AUTH_PASSWORD']
+enable :sessions
+
+USERNAME = 'mcpanel'
+
+def authenticate_user(password)
+  password == ENV['MCPANEL_AUTH_PASSWORD']
+end
+
+def logged_in
+  session['username']
 end
 
 helpers do
@@ -15,6 +23,8 @@ helpers do
 end
 
 get '/' do
+  redirect '/login' unless logged_in
+  
   ec2 = ec2_handle
   
   result = ec2.describe_instances(:instance_id => ENV['MC_EC2_INSTANCE_ID'])
@@ -26,7 +36,28 @@ end
 
 get '/start_server' do
   ec2_handle.start_instances(:instance_id => ENV['MC_EC2_INSTANCE_ID'])
-  "Server has been started. It will be ready shortly."
+  "Server has been started. It will be ready in a couple minutes."
+end
+
+get '/logout' do
+  session['username'] = nil
+  
+  redirect '/login'
+end
+
+get '/login' do
+  redirect '/' if logged_in
+  
+  haml :login
+end
+
+post '/login' do
+  if authenticate_user(params[:password])
+    session['username'] = USERNAME
+    redirect '/'
+  else
+    redirect '/login'
+  end
 end
 
 get '/stylesheet.css' do
